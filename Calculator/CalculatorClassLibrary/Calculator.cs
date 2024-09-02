f84e9292c50208b382e2ed9098be98747e7a5df5
NikolajVE <150657373+NikolajVE@users.noreply.github.com>
2024-09-02 14:45:10 +0200

    Add files via upload

---
+++ Calculator/CalculatorClassLibrary/Calculator.cs
@@ -0,0 +1,7 @@
+﻿namespace CalculatorClassLibrary
+{
+    public class Calculator
+    {
+
+    }
+}
---
+++ Calculator/Console Application/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Text.RegularExpressions;
+using Calc = CalculatorClassLibrary;
+namespace Console_Application;
+
+internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Calc.Calculator calculator = new Calc.Calculator();
+
+        if (!IsCorrectInput(args))
+        {
+            return;
+        }
+
+            
+
+
+            // Act
+
+            // Assert
+
+
+        }
+
+    private static bool IsCorrectInput(String[] args)
+    {
+        bool result = false;
+
+        Regex regex = new Regex(@"[0-9][\/\+\-\*][0-9]");
+
+        bool res = regex.IsMatch(args[0]);
+
+        return result;
+    }
+}
